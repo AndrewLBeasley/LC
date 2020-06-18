e56c7069cd571d4a12bf28b0493432f8979c209c
Andrew Beasley <alloydbeasley@gmail.com>
2020-06-18 15:42:26 -0500

    roman to integer with .reduce

--- 13.roman-to-integer.js
+++ 13.roman-to-integer.js
@@ -11,7 +11,7 @@
  */
 var romanToInt = function (s) {
     const numArr = s.split("");
-
+    
     const numerals = {
         I: 1,
         V: 5,
@@ -21,11 +21,8 @@ var romanToInt = function (s) {
         D: 500,
         M: 1000
     };
-    
-    let total = 0;
-
-    numArr.forEach((num, i, arr) => numerals[num] < numerals[arr[i + 1]] ? total += numerals[num] * -1 : total += numerals[num]);
-    return total;
+        
+    return numArr.reduce((acc, num, i, arr) =>( numerals[num] < numerals[arr[i + 1]] ? acc += numerals[num] * -1 : acc += numerals[num]), 0)    
 };
 // @lc code=end
 
